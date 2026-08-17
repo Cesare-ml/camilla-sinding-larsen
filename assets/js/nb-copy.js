1439d4a1f0ec94410839a52a64b27a587665a32a
Mirko Lancerotto <mirko.lance@gmail.com>
2026-08-17 18:56:57 +0200

    feat: add Norwegian tour copy

---
+++ assets/js/nb-copy.js
@@ -0,0 +1,34 @@
+window.TOUR_COPY = {
+  pitti: {
+    title: 'Palazzo Pitti og Bobolihagen',
+    body: 'Et besøk i Medici-familiens tidligere residens, gjennom dekorerte saler og den viktige malerisamlingen, før turen avsluttes med en spasertur i Bobolihagen og utsikt over Firenze.'
+  },
+  florence: {
+    title: 'Byvandring i Firenzes sentrum',
+    body: 'En introduksjon til Firenze til fots gjennom domkirkeområdet, middelaldergatene, den sivile arkitekturen og Ponte Vecchio. Turen knytter monumentene til familiene, verkstedene og historiene som formet byen.'
+  },
+  markets: {
+    title: 'Byvandring med en tur til markedet',
+    body: 'En byvandring som kombinerer Firenzes historie med matkultur og hverdagsliv. Sammen med de viktigste severdighetene utforsker vi markedets atmosfære og tradisjonene bak toskanske råvarer og retter.'
+  },
+  uffizi: {
+    title: 'Uffiziene',
+    body: 'Et konsentrert besøk i Uffiziene med fokus på sentrale renessanseverk, historiene bak dem, symbolikken, oppdragsgiverne og kunstnernes teknikker. Camillas bakgrunn som kunstkonservator gir også et praktisk blikk på hvordan verkene faktisk ble til.'
+  },
+  cathedral: {
+    title: 'Santa Maria del Fiore og domkirkeområdet',
+    body: 'Opplev domkirkeområdet med museum, baptisterium og Brunelleschis ekstraordinære kuppel. Besøket setter arkitektur, skulptur og ingeniørkunst inn i Firenzes større historie.'
+  },
+  siena: {
+    title: 'Byvandring i Siena',
+    body: 'Oppdag Sienas middelaldergater, Piazza del Campo, domkirken og tradisjonene som fortsatt preger byen. Ruten kan tilpasses tiden dere har til rådighet og hva dere er mest interessert i.'
+  },
+  towns: {
+    title: 'Toskanske byer',
+    body: 'Toscana er mye mer enn Firenze. Turen kan inkludere historiske steder som Siena, Lucca, Pisa, San Gimignano eller Volterra og kombinere kunst, arkitektur, lokale tradisjoner, mat og landskap.'
+  },
+  countryside: {
+    title: 'En dag på den toskanske landsbygda',
+    body: 'En roligere opplevelse utenfor de store byene, med landsbyer, villaer, olivenlunder, lokale mattradisjoner og selve landskapet. Dagen kan tilpasses område, årstid og gruppens interesser.'
+  }
+};
